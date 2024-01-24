0612cc962b93050d6ec9503de69264a5d6e65e37
Nisha Nambissan <namnbissan.nisha@gmail.com>
2024-01-24 09:11:20 +0000

    tidy up tests and add failing test for discount logic

--- PriceCalculator/PriceCalculatorTests/PricingCalculatorTests.cs
+++ PriceCalculator/PriceCalculatorTests/PricingCalculatorTests.cs
@@ -4,15 +4,30 @@ namespace PriceCalculatorTests;
 
 public class PricingCalculatorTests
 {
-    [Fact]
-    public void ShouldCalculateBasketTotalNoDiscountsApplicable()
+    [Theory]
+    [InlineData(1, 1, 1, "2.95")]
+    [InlineData(2, 3, 4, "9.05")]
+    public void ShouldCalculateBasketTotalNoDiscountsApplicable(int quantity1, int quantity2, int quantity3, string expectedTotal)
     {
         var basket = new Basket();
-        basket.Items.Add(new BasketProduct("butter", "Butter", 1, 0.8m));
-        basket.Items.Add(new BasketProduct("milk", "Milk", 1, 1.15m));
-        basket.Items.Add(new BasketProduct("bread", "Bread", 1, 1.0m));
+        basket.Items.Add(new BasketProduct("butter", "Butter", quantity1, 0.8m));
+        basket.Items.Add(new BasketProduct("milk", "Milk", quantity2, 1.15m));
+        basket.Items.Add(new BasketProduct("bread", "Bread", quantity3, 1.0m));
         
         var totalCost = basket.CalculateTotal();
-        Assert.Equal(2.95m, totalCost);
+
+        //had to do this because inlinedata would throw error otherwise [CS0182] 
+        Assert.Equal(decimal.Parse(expectedTotal), totalCost);
+    }
+    
+    [Fact]
+    public void ShouldCalculateBasketTotal_Bread_50Percent_Discounted_For2Butters()
+    {
+        var basket = new Basket();
+        basket.Items.Add(new BasketProduct("butter", "Butter", 2, 0.8m));
+        basket.Items.Add(new BasketProduct("bread", "Bread", 2, 1.0m));
+        
+        var totalCost = basket.CalculateTotal();
+        Assert.Equal(3.10m, totalCost);
     }
 }
